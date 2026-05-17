b3f78ef9d65091ca3185f55d65619deeca5d4ed0
dpkkmr515-oss <dpkkmr515@gmail.com>
2026-05-17 17:34:22 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_groq import ChatGroq
+
+# load the env variables
+load_dotenv()
+
+# page setup
+st.set_page_config(
+    page_title="🗨️ChatBot",
+    page_icon="🤖",
+    layout="centered",
+)
+st.title("🗨️Generative AI Chat Bot")
+
+#initiate chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# show chat history
+for message in st.session_state.chat_history:
+    with st.chat_history(message["role"]):
+        st.markdown(message["content"])
+
+#llm initiate
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0.0,
+)
+#inputbox
+user_prompt = st.chat_input("Ask to chatbot.....")
+if user_prompt:
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role":"user","content":user_prompt})
+    response = llm.invoke(
+        input = [{"role":"system","content":"You are a helpful assistant"}, *st.session_state.chat_history]
+    )
+    assistant_response =response.content
+    st.session_state.chat_history.append({"role":"assistant","content":assistant_response})
+
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
+
